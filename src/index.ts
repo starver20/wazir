e440301b96512932bbccc6275f8862476f07cc3d
amarnarute <amarnarute2002@gmail.com>
2021-01-29 10:37:52 +0530

    Iterate over json

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,18 @@ import jss from "json-stringify-safe";
 const app = express();
 
 app.use("/", async (req: Request, res: Response) => {
-  const data = await axios.get("https://api.wazirx.com/api/v2/tickers");
+  const data = (await axios.get("https://api.wazirx.com/api/v2/tickers")).data;
 
-  const info = data.data;
+  const keys = Object.keys(data);
 
-  res.send(info);
+  let array = [];
+  console.log(data[keys[2]]);
+  for (var i = 0; i < 10; i++) {
+    var key = keys[i];
+    array.push(data[key]);
+  }
+
+  res.send(array);
 });
 
 app.listen(3000, () => {
