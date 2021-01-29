a8f908841bc5e15de8979557502aa9b820669a27
amarnarute <amarnarute2002@gmail.com>
2021-01-29 10:16:09 +0530

    Set up express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import axios from "axios";
+import jss from "json-stringify-safe";
 
 const app = express();
+
+app.use("/", async (req: Request, res: Response) => {
+  const data = await axios.get("https://api.wazirx.com/api/v2/tickers");
+
+  const info = data.data;
+
+  res.send(info);
+});
 
 app.listen(3000, () => {
   console.log("listening on 3000");
